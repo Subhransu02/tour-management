626fda16072a9dc0e3dd3d147207b22e27b7b591
Subhransu sekhar panda <subhransusekharpanda02@gmail.com>
2023-12-30 02:22:30 +0530

    add home and search bar

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,6 +6,8 @@ import heroImg02 from "../assets/images/hero-img02.jpg";
 import heroVideo from "../assets/images/hero-video.mp4";
 import Subtitle from "../shared/Subtitle";
 import worldImg from "../assets/images/world.png";
+import SearchBar from "../shared/SearchBar";
+import ServiceList from "../services/ServiceList";
 
 const Home = () => {
   return (
@@ -40,19 +42,31 @@ const Home = () => {
               </div>
             </Col>
             <Col lg="2">
-              <div className="hero__img-box">
+              <div className="hero__img-box mt-4">
                 <video src={heroVideo} alt="" controls />
               </div>
             </Col>
             <Col lg="2">
-              <div className="hero__img-box">
+              <div className="hero__img-box mt-5">
                 <img src={heroImg02} alt="" />
               </div>
             </Col>
+            <SearchBar />
           </Row>
         </Container>
       </section>
       {/*hero section end */}
+      <section>
+        <Container>
+          <Row>
+            <Col lg="3">
+              <h5 className="services__subtitle">What we serve</h5>
+              <h2 className="services__title">We offer our best services</h2>
+            </Col>
+            <ServiceList />
+          </Row>
+        </Container>
+      </section>
     </>
   );
 };
