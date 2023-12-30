ae3d7ebe77ec2561f6815346b87d296e9ebef0a8
Subhransu sekhar panda <subhransusekharpanda02@gmail.com>
2023-12-30 23:05:18 +0530

    add image section

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -8,7 +8,9 @@ import Subtitle from "../shared/Subtitle";
 import worldImg from "../assets/images/world.png";
 import SearchBar from "../shared/SearchBar";
 import ServiceList from "../services/ServiceList";
-
+import FeaturedTourList from "../Featured-tours/FeaturedTourList";
+import experienceImg from "../assets/images/experience.png";
+import MasonryImagesGallery from "../components/Image-gallery/MasonryImagesGallery";
 const Home = () => {
   return (
     <>
@@ -67,6 +69,84 @@ const Home = () => {
           </Row>
         </Container>
       </section>
+
+      {/*featured tour section start */}
+      <section>
+        <Container>
+          <Row>
+            <Col lg="12" className="mb-5">
+              <Subtitle subtitle={"Explore"} />
+              <h2 className="featured__tour-title">Our featured tours</h2>
+            </Col>
+            <FeaturedTourList />
+          </Row>
+        </Container>
+      </section>
+
+      {/*featured tour section end */}
+      {/* experience section start*/}
+
+      <section>
+        <Container>
+          <Row>
+            <Col lg="6">
+              <div className="experience__content">
+                <Subtitle subtitle={"Experience"} />
+                <h2>
+                  With our all experience <br /> we will serve you
+                </h2>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis
+                  voluptates, repellat, voluptas, quod quae voluptatibus
+                  consequatur iusto molestias voluptate natus doloribus. Quis
+                  voluptates, repellat, voluptas, quod quae voluptatibus
+                  consequatur iusto molestias voluptate natus doloribus.
+                </p>
+              </div>
+
+              <div className="counter__wrapper d-flex align-items-center gap-5">
+                <div className="counter__box">
+                  <span>12k+</span>
+                  <h6>Successful trip</h6>
+                </div>
+                <div className="counter__box">
+                  <span>2k+</span>
+                  <h6>Regular Clients</h6>
+                </div>
+                <div className="counter__box">
+                  <span>15</span>
+                  <h6>Years experience</h6>
+                </div>
+              </div>
+            </Col>
+            <Col lg="6">
+              <div className="experience__img">
+                <img src={experienceImg} alt="" />
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+
+      {/* experience section end*/}
+
+      {/*gallery section start */}
+      <section>
+        <Container>
+          <Row>
+            <Col lg="12">
+              <Subtitle subtitle={"Gallery"} />
+              <h2 className="gallery__title">
+                Visit our customers tour gallery
+              </h2>
+            </Col>
+            <Col lg="12">
+              <MasonryImagesGallery />
+            </Col>
+          </Row>
+        </Container>
+      </section>
+      {/*gallery section end */}
     </>
   );
 };
