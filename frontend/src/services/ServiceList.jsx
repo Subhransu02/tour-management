096b151cfa52e1658f21c7ac2a40eba1cd70209b
Subhransu sekhar panda <subhransusekharpanda02@gmail.com>
2023-12-30 14:04:36 +0530

    add service card, list and search bar

---
+++ frontend/src/services/ServiceList.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import ServiceCard from "./ServiceCard";
+import { Col } from "reactstrap";
+import weatherImg from "../assets/images/weather.png";
+import guideImg from "../assets/images/guide.png";
+import customizationImg from "../assets/images/customization.png";
+
+const servicesData = [
+  {
+    imgUrl: weatherImg,
+    title: "Weather",
+    desc: "Weather service description",
+  },
+  {
+    imgUrl: guideImg,
+    title: "Guide",
+    desc: "Guide service description",
+  },
+  {
+    imgUrl: customizationImg,
+    title: "Customization",
+    desc: "Customization service description",
+  },
+];
+
+const ServiceList = () => {
+  return (
+    <>
+      {servicesData.map((item, index) => (
+        <Col lg="3" key={index}>
+          <ServiceCard item={item} />
+        </Col>
+      ))}
+    </>
+  );
+};
+
+export default ServiceList;
