037acab354306a65348f4ca1cc3bb8e8075fd6ea
Subhransu sekhar panda <subhransusekharpanda02@gmail.com>
2024-01-10 03:37:01 +0530

    add backend routes and tourcontrollers

---
+++ backend/routes/tours.js
@@ -0,0 +1,17 @@
+import express from "express";
+import {
+  createTour,
+  deleteTour,
+  getAllTour,
+  getSingleTour,
+  updateTour,
+} from "./../Controllers/tourController.js";
+const router = express.Router();
+
+router.post("/", createTour);
+router.put("/:id", updateTour);
+router.delete("/:id", deleteTour);
+router.get("/:id", getSingleTour);
+router.get("/", getAllTour);
+
+export default router;
