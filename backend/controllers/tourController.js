468d10a2256e34d04376833a15c6acb8a951a8ca
Subhransu sekhar panda <subhransusekharpanda02@gmail.com>
2024-01-10 17:54:28 +0530

    add tour controllers

--- backend/controllers/tourController.js
+++ backend/controllers/tourController.js
@@ -81,18 +81,65 @@ export const getAllTour = async (req, res) => {
     const tours = await Tour.find({})
       .skip(page * 8)
       .limit(8);
-    res
-      .status(200)
-      .json({
-        success: true,
-        count: tours.length,
-        message: "Successful",
-        data: tours,
-      });
+    res.status(200).json({
+      success: true,
+      count: tours.length,
+      message: "Successful",
+      data: tours,
+    });
   } catch (error) {
     res.status(404).json({
       success: false,
       message: "Not Found",
     });
+  }
+};
+
+export const getTourBySearch = async (req, res) => {
+  const city = new RegExp(req.query.city, "i");
+  const distance = parseInt(req.query.distance);
+  const maxGroupSize = parseInt(req.query.maxGroupSize);
+
+  try {
+    const tours = await Tour.find({
+      city,
+      distance: { $gte: distance },
+      maxGroupSize: { $gte: maxGroupSize },
+    });
+    res.status(200).json({
+      success: true,
+      message: "Successful",
+      data: tours,
+    });
+  } catch (error) {
+    res.status(404).json({
+      success: false,
+      message: "Not Found",
+    });
+  }
+};
+
+export const getFeaturedTours = async (req, res) => {
+  try {
+    const tours = await Tour.find({ featured: true }).limit(8);
+    res.status(200).json({
+      success: true,
+      message: "Successful",
+      data: tours,
+    });
+  } catch (error) {
+    res.status(404).json({
+      success: false,
+      message: "Not Found",
+    });
+  }
+};
+
+export const getTourCount = async (req, res) => {
+  try {
+    const tourCount = await Tour.estimatedDocumentCount();
+    res.status(200).json({ success: true, data: tourCount });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "failed to fetch" });
   }
 };
--- backend/routes/tours.js
+++ backend/routes/tours.js
@@ -3,7 +3,10 @@ import {
   createTour,
   deleteTour,
   getAllTour,
+  getFeaturedTours,
   getSingleTour,
+  getTourBySearch,
+  getTourCount,
   updateTour,
 } from "./../Controllers/tourController.js";
 const router = express.Router();
@@ -13,5 +16,8 @@ router.put("/:id", updateTour);
 router.delete("/:id", deleteTour);
 router.get("/:id", getSingleTour);
 router.get("/", getAllTour);
+router.get("/search/getToursBySearch", getTourBySearch);
+router.get("/search/getFeaturedTours", getFeaturedTours);
+router.get("/search/getTourCount", getTourCount);
 
 export default router;
