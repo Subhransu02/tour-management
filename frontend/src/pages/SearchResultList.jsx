6ae4427922e9ff149133fa43d0290cf72dad466b
Subhransu sekhar panda <subhransusekharpanda02@gmail.com>
2024-01-12 17:25:15 +0530

    add search result

--- frontend/src/pages/SearchResultList.jsx
+++ frontend/src/pages/SearchResultList.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useState } from "react";
+import CommonSection from "./../shared/CommonSection";
+import { Container, Row, Col } from "reactstrap";
 
+import { useLocation } from "react-router-dom"; // Add missing import statement
+import TourCard from "./../shared/TourCard";
+import Newsletter from "./../shared/Newsletter"
 const SearchResultList = () => {
+  const location = useLocation();
+  const [data] = useState(location.state.data);
+  //console.log(data);
+  
   return (
-    <div>SearchResultList</div>
-  )
-}
+    <>
+      <CommonSection title="Search Result List" />
+      <section>
+        <Container>
+          <Row>
+            {data.length === 0 ? (
+              <h4 className="text-center">No tour found</h4>
+            ) : (
+              data?.map((tour) => (
+                <Col lg="3" className="mb-4" key={tour._id}>
+                  <TourCard tour={tour} />
+                </Col>
+              ))
+            )}
+          </Row>
+        </Container>
+      </section>
+      <Newsletter />
+    </>
+  );
+};
 
-export default SearchResultList
+export default SearchResultList;
